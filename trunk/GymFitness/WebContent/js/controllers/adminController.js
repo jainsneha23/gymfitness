6b08d758f7e46381ea94e5eece74d1a9323a3b17
mystery.sneha@gmail.com <mystery.sneha@gmail.com@aa628d0c-e05a-1edc-6dab-2d435750280e>
2014-12-03 17:09:13 +0000

    bootstrap fonts added. form add del done

--- trunk/GymFitness/WebContent/js/controllers/adminController.js
+++ trunk/GymFitness/WebContent/js/controllers/adminController.js
@@ -1,6 +1,31 @@
 gymApp.controller('gymController', function($scope, $http) {
+
+	activeTab = 0;
+
     $scope.activities_model = model.activities;
     $scope.equipments_model = model.equipments;
+
+    $scope.model = [$scope.activities_model,$scope.equipments_model];
+
+    $scope.editItems = false;
+    $scope.form = {
+    	name : '',
+    	description : ''
+    };
+
+    $scope.addItem = function(){
+    	var item = {
+    		name : $scope.form.name,
+    		description : $scope.form.description
+    	};
+    	$scope.model[activeTab].push(item);
+    	$scope.form = {};
+    };
+
+    $scope.deleteItem = function(index) {
+    	$scope.model[activeTab].splice(index,1);
+    }
+
 });
 gymApp.controller('employeeController', function($scope, $http) {});
 gymApp.controller('customerController', function($scope, $http) {});
