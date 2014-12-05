f06afc1891be989d303eba8a74176ba16bbf79dd
mystery.sneha@gmail.com <mystery.sneha@gmail.com@aa628d0c-e05a-1edc-6dab-2d435750280e>
2014-12-05 11:29:44 +0000

    employee table, delete and add option done

--- trunk/GymFitness/WebContent/js/controllers/adminController.js
+++ trunk/GymFitness/WebContent/js/controllers/adminController.js
@@ -1,4 +1,4 @@
-gymApp.controller('gymController', function($scope, $http) {
+gymApp.controller('gymController', function($scope) {
 
     $scope.tabModel = [{
         name: 'Activities',
@@ -55,5 +55,89 @@ gymApp.controller('gymController', function($scope, $http) {
     }
 
 });
-gymApp.controller('employeeController', function($scope, $http) {});
-gymApp.controller('customerController', function($scope, $http) {});
+gymApp.controller('employeeController', function($scope, $modal) {
+
+    $scope.employeeModel = model.employeeModel;
+
+    $scope.allSelected = false;
+
+    for (var i = 0; i < $scope.employeeModel.length; i++)
+        $scope.employeeModel[i].selected = false;
+
+    $scope.selectAll = function(value) {
+        if (typeof value != "undefined") {
+            $scope.employeeModel.map(function(obj) {
+                obj.selected = value;
+                return obj;
+            });
+        } else {
+            $scope.allSelected = $scope.employeeModel.every(function(obj) {
+                return obj.selected;
+            });
+        }
+    }
+
+    $scope.deleteItems = function() {
+        if ($scope.editItems)
+            $scope.employeeModel = $scope.employeeModel.filter(function(obj) {
+                return obj.selected == false;
+            });
+    }
+
+    $scope.addItem = function() {
+
+        var modalInstance = $modal.open({
+            templateUrl: 'addEmployee.html',
+            controller: 'registerController'
+        });
+
+        modalInstance.result.then(function(obj) {
+            $scope.employeeModel.push(obj);
+        }, function() {});
+    }
+});
+gymApp.controller('customerController', function($scope) {});
+
+gymApp.controller('registerController', function($scope, $modalInstance) {
+
+    $scope.message = '';
+
+    $scope.registerFormData = {
+        fname: "",
+        lname: "",
+        contact: "",
+        email: "",
+        address: "",
+        city: "",
+        state: "",
+        country: "",
+        pin: "",
+        dob: "",
+        gender: "",
+        doj: "",
+        salary: "",
+        lastpaid: ""
+    };
+
+    $scope.cancel = function(){
+        $modalInstance.dismiss('cancel');
+    }
+
+    $scope.registerUser = function(form) {
+
+        if (!form.$dirty || !form.$valid) {
+            $scope.message = "Please fill up all fields";
+            return;
+        }
+        $modalInstance.close($scope.registerFormData);
+
+        /*$http.post('UserRegister', [$scope.registerFormData])
+            .success(function(data, status, headers, config) {
+                $scope.message = 'We added u';
+            }).error(function(data, status, headers, config) {
+                $scope.message = 'Error occured : ' + data;
+            });*/
+
+    };
+
+});
