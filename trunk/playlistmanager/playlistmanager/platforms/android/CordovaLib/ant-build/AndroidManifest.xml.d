aaea89a4657795d386261200584bd215681319db
mystery.sneha@gmail.com <mystery.sneha@gmail.com@aa628d0c-e05a-1edc-6dab-2d435750280e>
2014-12-15 06:13:14 +0000

    initial commit

---
+++ trunk/playlistmanager/playlistmanager/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\opg\Eclipse_workspace\playlistmanager\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\opg\Eclipse_workspace\playlistmanager\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ trunk/playlistmanager/playlistmanager/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\opg\Eclipse_workspace\playlistmanager\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\opg\Eclipse_workspace\playlistmanager\platforms\android\AndroidManifest.xml \
