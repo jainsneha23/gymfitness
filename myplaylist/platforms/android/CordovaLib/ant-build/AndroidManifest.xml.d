390a4ea71abdf526a455bb7b0e403f201e9017c7
Sneha Jain <snehatulsi@users.noreply.github.com>
2015-04-04 09:17:26 +0530

    deleted playlist

--- myplaylist/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\opg\Eclipse_workspace\playlistmanager\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\opg\Eclipse_workspace\playlistmanager\platforms\android\CordovaLib\AndroidManifest.xml \
